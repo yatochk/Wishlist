66ee92fdca8f518e96edf35954265a9c5fe36040
a.zotov <a.zotov@tinkoff.ru>
2022-01-28 16:58:47 +0300

    sorting

--- app/src/main/java/com/yatochk/wishlistapp/domain/GetUserWishListUseCase.kt
+++ app/src/main/java/com/yatochk/wishlistapp/domain/GetUserWishListUseCase.kt
@@ -4,6 +4,7 @@ import com.yatochk.wishlist.gifts.api.gift.data.Gift
 import com.yatochk.wishlist.gifts.api.gift.data.GiftsRepository
 import com.yatochk.wishlistapp.login.api.data.UserInfoRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetUserWishListUseCase @Inject constructor(
@@ -13,7 +14,9 @@ class GetUserWishListUseCase @Inject constructor(
 
     fun get(): Flow<List<Gift>> {
         val wishListName = userInfoRepository.userInfo?.wishListName.orEmpty()
-        return giftsRepository.getGiftsByName(wishListName)
+        return giftsRepository.getGiftsByName(wishListName).map { list ->
+            list.sortedBy { it.name }
+        }
     }
 
 }
